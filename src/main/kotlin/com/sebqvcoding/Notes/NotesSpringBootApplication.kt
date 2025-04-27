65bccc9d0082a6e72bccf404c049ac0c3422897f
techConsul97 <ittechconsultantemail@gmail.com>
2025-04-27 15:39:50 +0100

    first commit
    adding the default template for using SpringBoot

---
+++ src/main/kotlin/com/sebqvcoding/Notes/NotesSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package com.sebqvcoding.Notes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotesSpringBootApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotesSpringBootApplication>(*args)
+}
---
+++ src/test/kotlin/com/sebqvcoding/Notes/NotesSpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sebqvcoding.Notes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotesSpringBootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
